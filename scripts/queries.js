9b2401e61373fb3140eb5468b06fcb7211bf7005
matdexir <matthieudesir21@gmail.com>
2022-05-31 22:03:45 +0800

    Refactor: Moved to jQuery terminal

--- scripts/queries.js
+++ scripts/queries.js
@@ -1,22 +1,18 @@
-let command = document.getElementById("command");
-let block_div = document.getElementById("output-block");
-command.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-		let input = command.value
-		let found = false
-		for (let i = 0; i < commands.length; i++) {
-			if(commands[i].name === input){
-				found = true
-			  let output = document.createElement("div")
-			  output.innerHTML = '<h1>' + commands[i].description + '</h1>'
-			  console.log(output)
-			  // block_div.insertBefore(output, block_div.firstChild)
-			  block_div.appendChild(output)
-			  console.log(block_div)
-			}
-		}
-		if (!found)
-			console.log("Command not found")
-		command.value = "";
+$("body").terminal(
+  {
+    hello: function (what) {
+      this.echo("Hello, " + what + ". Welcome to this terminal.");
+    },
+    cat: function(width, height) {
+        const img = $('<img src="https://placekitten.com/' +
+                      width + '/' + height + '">');
+        img.on('load', this.resume);
+        this.pause();
+        this.echo(img);
+        this.echo(img);
+    },
+  },
+  {
+    greetings: greetings.innerHTML,
   }
-});
+);
