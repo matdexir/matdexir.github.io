53002a98217a59fe805dfd733684e17ae9a26976
matdexir <matthieudesir21@gmail.com>
2022-05-31 16:38:34 +0800

    Fix: Use correct order for command output

--- scripts/queries.js
+++ scripts/queries.js
@@ -1,5 +1,5 @@
 let command = document.getElementById("command");
-let block_div = document.getElementById("command-block");
+let block_div = document.getElementById("output-block");
 command.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
 		let input = command.value
@@ -7,12 +7,11 @@ command.addEventListener("keypress", function (e) {
 		for (let i = 0; i < commands.length; i++) {
 			if(commands[i].name === input){
 				found = true
-				console.log(commands[i].description)
-			  output = document.createElement("div")
-			  output.innerHTML = '<h1>' + "This is a placeholder" + '</h1>'
+			  let output = document.createElement("div")
+			  output.innerHTML = '<h1>' + commands[i].description + '</h1>'
 			  console.log(output)
-			  block_div.insertBefore(output, block_div.firstChild)
-			  // block_div.appendChild(output)
+			  // block_div.insertBefore(output, block_div.firstChild)
+			  block_div.appendChild(output)
 			  console.log(block_div)
 			}
 		}
