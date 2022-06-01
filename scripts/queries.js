fe6e7dda8a944dccd2f7c3b00071d05ba85d0e87
matdexir <matthieudesir21@gmail.com>
2022-06-01 11:55:54 +0800

    Testing out new features

--- scripts/queries.js
+++ scripts/queries.js
@@ -1,18 +1,32 @@
+let count = 0
 $("body").terminal(
   {
     hello: function (what) {
       this.echo("Hello, " + what + ". Welcome to this terminal.");
     },
-    cat: function(width, height) {
-        const img = $('<img src="https://placekitten.com/' +
-                      width + '/' + height + '">');
-        img.on('load', this.resume);
-        this.pause();
-        this.echo(img);
-        this.echo(img);
+    cat: function (width, height) {
+      const img = $(
+        '<img src="https://placekitten.com/' + width + "/" + height + '">'
+      );
+      img.on("load", this.resume);
+      this.pause();
+      this.echo(img);
+      this.echo(img);
+    },
+    name: function (name) {
+      this.read("last name:", (last_name) => {
+        this.echo("Your name is " + name + " " + last_name);
+      });
     },
   },
   {
     greetings: greetings.innerHTML,
+    prompt: "matdexir@about> ",
+    keymap: {
+      "ENTER": function (e, original) {
+          original();
+          this.set_prompt(++count + ">")
+      },
+    },
   }
 );
