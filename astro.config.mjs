1da9e1c2d7d404cb8192ea0179d3e83070fd78a5
matdexir <matthieudesir21@gmail.com>
2023-01-23 23:24:53 +0800

    transferred index page into a markdown file

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,4 +14,18 @@ export default defineConfig({
   integrations: [tailwind(), react(), image()],
   site: "https://matdexir.github.io",
   base: "/",
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula-soft",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      // langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
