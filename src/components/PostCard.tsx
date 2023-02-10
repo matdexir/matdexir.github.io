3714451722a99c2537674ba0e5345ccc6504f584
matdexir <matthieudesir21@gmail.com>
2023-02-11 01:52:16 +0800

    fix: added button link for post pages with astro islands

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,7 @@
 interface PostCardProps {
   url: string;
   description: string;
+  id: string;
   title: string;
   tags: Array<string>;
   date: Date;
@@ -8,6 +9,7 @@ interface PostCardProps {
 export default function PostCard({
   url,
   description,
+  id,
   title,
   tags,
   date,
@@ -18,35 +20,32 @@ export default function PostCard({
     day: "numeric",
   };
   return (
-    <>
-      <a href={url} className="block">
-        <div className="rounded-md bg-green-600 my-8 px-3 py-3 dark:bg-green-900 mt-2 shadow-sm shadow-green-400 hover:scale-105 hover:-translate-y-1 duration-300">
-          <li>
-            <a href={url}>
-              <span className="ml-1 text-3xl">{title}</span>
-            </a>
-            <p className="ml-1 mt-2 text-zinc-800 dark:text-zinc-300">
-              {description}
-            </p>
-            <div className="my-2 flex gap-2">
-              {tags.map((tag: string) => (
-                <a href={`/tags/${tag.toLowerCase()}`}>
-                  <span className="inline-flex items-center gap-1 rounded-full text-xs bg-zinc-400 dark:bg-zinc-600 font-semibold px-2 py-1 hover:text-green-700">
-                    {tag}
-                  </span>
-                </a>
-              ))}
-            </div>
-            <p className="ml-1 text-xs font-thin text-zinc-900 dark:text-zinc-400">
-              Posted on
-              <span className="text-zinc-600 dark:text-zinc-300">
-                {" "}
-                {new Date(date).toLocaleDateString("en-US", dateOptions)}
-              </span>
-            </p>
-          </li>
-        </div>
+    <li key={id}>
+      <a href={url}>
+        <button className="rounded-md w-[100%] bg-green-600 my-8 px-3 py-3 dark:bg-green-900 mt-2 shadow-sm shadow-green-400 hover:scale-105 hover:-translate-y-1 duration-300">
+          <span className="ml-1 text-3xl  text-zinc-800 dark:text-zinc-400">
+            {title}
+          </span>
+          <p className="ml-1 mt-2 text-zinc-800 dark:text-zinc-300">
+            {description}
+          </p>
+          <div className="my-2 flex gap-2 items-center justify-center">
+            {tags.map((tag: string) => (
+              <a href={`/tags/${tag.toLowerCase()}`}>
+                <span className="inline-flex items-center gap-1 rounded-full text-xs bg-zinc-400 dark:bg-zinc-600 font-semibold px-2 py-1 hover:text-green-700">
+                  {tag}
+                </span>
+              </a>
+            ))}
+          </div>
+          <p className="ml-1 text-xs font-thin text-zinc-900 dark:text-zinc-400">
+            Posted on
+            <span className="text-zinc-700 dark:text-zinc-300 ml-1">
+              {new Date(date).toLocaleDateString("en-US", dateOptions)}
+            </span>
+          </p>
+        </button>
       </a>
-    </>
+    </li>
   );
 }
