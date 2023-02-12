127e15555a462f357f8112ee90e940ed91f8eada
matdexir <matthieudesir21@gmail.com>
2023-02-12 23:11:07 +0800

    feat(scrolltotop.tsx): added small animation to ScrollToTop button

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,8 +1,12 @@
 import { AiOutlineArrowUp } from "react-icons/ai/index";
-import { useState, useEffect, Fragment } from "react";
-// import { Transition } from "react-transition-group/";
+import { useState, useEffect, Fragment, useRef } from "react";
+import { Transition, TransitionStatus } from "react-transition-group/";
+
+import "../styles/transition.css";
+
 export default function ScrollToTop() {
   const [showButton, setShowButton] = useState(false);
+  const nodeRef = useRef(null);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.pageYOffset > 100) {
@@ -13,20 +17,54 @@ export default function ScrollToTop() {
     });
   }, []);
   const handleScrollToTop = () => {
-    console.log("Hello");
+    // console.log("Hello");
     window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const duration = 900;
+
+  const defaultStyle = {
+    transition: `transform ${duration}ms, opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+  };
+
+  const transitionStyles: any = {
+    entering: { opacity: 0.5, transform: "rotate(90deg)" },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0.5, transform: "rotate(90deg)" },
+    exited: { opacity: 0 },
   };
 
   return (
     <Fragment>
-      {showButton && (
-        <button
-          className={`fixed bottom-5 right-7 z-50 cursor-pointer p-4 bg-green-500 dark:bg-green-600 rounded-full`}
-          onClick={handleScrollToTop}
+      {
+        <Transition
+          in={showButton}
+          timeout={1000}
+          appear={showButton}
+          nodeRef={nodeRef}
+          // onExiting={() => {
+          //   console.log("exiting");
+          // }}
+          // onExited={() => {
+          //   console.log("exit complete");
+          // }}
+          unmountOnExit
         >
-          <AiOutlineArrowUp />
-        </button>
-      )}
+          {(state: TransitionStatus) => (
+            <button
+              className={`fixed bottom-5 right-7 z-50 cursor-pointer p-4 bg-green-500 dark:bg-green-600 rounded-full`}
+              onClick={handleScrollToTop}
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state],
+              }}
+            >
+              <AiOutlineArrowUp />
+            </button>
+          )}
+        </Transition>
+      }
     </Fragment>
   );
 }
