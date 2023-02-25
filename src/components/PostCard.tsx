d3844d378d0c19ff80b556efc097c48a938a55ee
matdexir <matthieudesir21@gmail.com>
2023-02-25 21:05:12 +0800

    fix: remove nested links and added tags list to each page of the blog posts

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -23,19 +23,17 @@ export default function PostCard({
     <li key={id}>
       <a href={url}>
         <button className="rounded-md w-[100%] bg-green-600 my-8 px-3 py-3 dark:bg-green-900 mt-2 shadow-sm shadow-green-400 hover:scale-105 hover:-translate-y-1 duration-300">
-          <span className="ml-1 text-3xl  text-zinc-800 dark:text-zinc-400">
+          <span className="ml-1 text-3xl  text-zinc-800 dark:text-yellow-200">
             {title}
           </span>
-          <p className="ml-1 mt-2 text-zinc-800 dark:text-zinc-300">
+          <p className="ml-1 mt-2 text-zinc-800 dark:text-zinc-100">
             {description}
           </p>
           <div className="my-2 flex gap-2 items-center justify-center">
             {tags.map((tag: string) => (
-              <a key={tag} href={`/tags/${tag.toLowerCase()}`}>
-                <span className="inline-flex items-center gap-1 rounded-full text-xs bg-zinc-400 dark:bg-zinc-600 font-semibold px-2 py-1 hover:text-green-700">
-                  {tag}
-                </span>
-              </a>
+              <span className="inline-flex items-center gap-1 rounded-full text-xs bg-zinc-400 dark:bg-zinc-600 font-semibold px-2 py-1">
+                {tag}
+              </span>
             ))}
           </div>
           <p className="ml-1 text-xs font-thin text-zinc-900 dark:text-zinc-400">
